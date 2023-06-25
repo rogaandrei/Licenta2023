77987a0ca2b99cb92fdc673184975ef9e6f64875
rogaandrei <109062431+rogaandrei@users.noreply.github.com>
2023-06-25 17:49:53 +0300

    Update Quit.cs

--- Scripturi/Scripturi pentru butoanele din scena/Quit.cs
+++ Scripturi/Scripturi pentru butoanele din scena/Quit.cs
@@ -5,13 +5,11 @@ public class Quit : MonoBehaviour
 {
     private void Start()
     {
-        // Înregistrează metoda QuitGame() ca răspuns la evenimentul OnClick() al dispozitivului de input
         GetComponent<UnityEngine.UI.Button>().onClick.AddListener(QuitGame);
     }
 
     private void QuitGame()
     {
-        // Închide aplicația
         Application.Quit();
     }
 }

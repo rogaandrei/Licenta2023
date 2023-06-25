06d31e2569a99b6dbd869ed2667828806dbe0d3e
rogaandrei <109062431+rogaandrei@users.noreply.github.com>
2023-06-25 17:51:14 +0300

    Update Diamante.cs

--- Scripturi/ScripturiMonede/Diamante.cs
+++ Scripturi/ScripturiMonede/Diamante.cs
@@ -2,14 +2,12 @@
 
 public class Diamante : MonoBehaviour
 {
-    public float rotationSpeed = 10f; // Viteza de rotație a obiectului pe axa Y
+    public float rotationSpeed = 10f; 
 
     private void Update()
     {
-        // Obține unghiurile curente de rotație
         Vector3 currentRotation = transform.eulerAngles;
 
-        // Setează doar rotația pe axa Y
         float newYRotation = currentRotation.y + (rotationSpeed * Time.deltaTime);
         transform.eulerAngles = new Vector3(currentRotation.x, newYRotation, currentRotation.z);
     }

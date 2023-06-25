d4263077797a3c4e3008cc5c08acaddbf38ffaee
rogaandrei <109062431+rogaandrei@users.noreply.github.com>
2023-06-25 17:51:27 +0300

    Update Monede_Invart.cs

--- Scripturi/ScripturiMonede/Monede_Invart.cs
+++ Scripturi/ScripturiMonede/Monede_Invart.cs
@@ -2,11 +2,9 @@
 
 public class Monede_Invart : MonoBehaviour
 {
-    public float rotationSpeed = 10f; // Viteza de rotație a obiectului pe axa Z
-
+    public float rotationSpeed = 10f;
     private void Update()
     {
-        // Realizează rotația continuă pe axa Z
         transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
     }
 }

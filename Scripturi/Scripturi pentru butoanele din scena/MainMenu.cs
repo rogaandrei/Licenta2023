29a10ca5654b11fc47eeda44eb413eab732cc334
rogaandrei <109062431+rogaandrei@users.noreply.github.com>
2023-06-25 17:48:36 +0300

    Update MainMenu.cs

--- Scripturi/Scripturi pentru butoanele din scena/MainMenu.cs
+++ Scripturi/Scripturi pentru butoanele din scena/MainMenu.cs
@@ -4,11 +4,10 @@ using UnityEngine.InputSystem;
 
 public class MainMenu : MonoBehaviour
 {
-    public string sceneName; // Numele scenei în care dorești să te arunci
+    public string sceneName; 
 
     private void Start()
     {
-        // Înregistrează metoda LoadSceneOnClick() ca răspuns la evenimentul OnClick() al dispozitivului de input
         GetComponent<UnityEngine.UI.Button>().onClick.AddListener(LoadSceneOnClick);
     }
 
